d8e75fe935161dd0c31222dc2b5b750ef1af1513
2rang2 <rang0825@gmail.com>
2024-05-10 18:44:12 +0900

    Initial Commit

---
+++ RangCard_SwiftUI/RangCard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RangCard_SwiftUIApp.swift
+//  RangCard_SwiftUI
+//
+//  Created by Rang Lee on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RangCard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
